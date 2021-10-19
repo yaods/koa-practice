e50db9a464eb079344d83006e8d9dd1c9b965f10
yaods <firmwin@qq.com>
2021-10-19 17:01:59 +0800

    add middleware koa-router

---
+++ index.js
@@ -0,0 +1,37 @@
+const koa = require('koa')
+const convert = require('koa-convert')
+const loggerGenerator = require('./middleware/logger-generator')
+const loggerAsync = require('./middleware/logger-async')
+const fs = require('fs')
+const app = new koa()
+
+const Router = require('koa-router')
+
+// app.use(convert(loggerGenerator()))
+// app.use(loggerAsync())
+// app.use( async(ctx) => {
+//     ctx.body = 'hello koa2'
+// })
+
+let home = new Router()
+home.get('/', async (ctx)=>{
+    let html = `
+        <ul>
+            <li><a href="/page/helloworld">/page/helloworld</a></li>
+            <li><a href="/page/404">/page/404</a></li>
+        </ul>
+    `
+    ctx.body = html
+})
+
+// 装载所有子路由
+let router = new Router()
+router.use('/', home.routes(), home.allowedMethods())
+// router.use('/page', page.routes(), page.allowedMethods())
+
+// 加载路由中间件
+app.use(router.routes()).use(router.allowedMethods())
+
+
+app.listen(3000) 
+console.log('[demo] start-quick is starting at port 3000')
