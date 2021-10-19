eb566985f3b7204fcdf39135e3e4b01cb408e1b6
yaods <firmwin@qq.com>
2021-10-19 17:08:27 +0800

    fix router-test

--- index.js
+++ index.js
@@ -24,10 +24,18 @@ home.get('/', async (ctx)=>{
     ctx.body = html
 })
 
+// 子路由2
+let page = new Router()
+page.get('/404', async ( ctx )=>{
+  ctx.body = '404 page!'
+}).get('/helloworld', async ( ctx )=>{
+  ctx.body = 'helloworld page!'
+})
+
 // 装载所有子路由
 let router = new Router()
 router.use('/', home.routes(), home.allowedMethods())
-// router.use('/page', page.routes(), page.allowedMethods())
+router.use('/page', page.routes(), page.allowedMethods())
 
 // 加载路由中间件
 app.use(router.routes()).use(router.allowedMethods())
