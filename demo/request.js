ac1c80b715c4fc231539ddf1924518083e7a2940
yaods <firmwin@qq.com>
2021-10-19 18:22:31 +0800

    koa-bodyparser

---
+++ demo/request.js
@@ -0,0 +1,25 @@
+const Koa = require('koa')
+const app = new Koa()
+
+app.use( async(ctx) => {
+    let url = ctx.url
+
+    let request = ctx.request
+    let req_query = request.query
+    let req_queryString = request.querystring
+
+    let ctx_query = ctx.query
+    let ctx_queryString = ctx.querystring
+
+    ctx.body = {
+        url,
+        req_query,
+        req_queryString,
+        ctx_query,
+        ctx_queryString
+    }
+})
+
+app.listen(3000, () => {
+    console.log('request get is starting at port 3000')
+})
